743ce736ab0fa0035ff11b578589f38091254972
Ian White <ian.white@t-3.com>
2016-08-15 11:53:32 -0500

    Crude attempt to Show zoomed into the state in the visual (other states should either not be visible or maybe greyed out if possible

--- client/lib/AddMapBoundaries.js
+++ client/lib/AddMapBoundaries.js
@@ -52,6 +52,21 @@ let AddMapBoundaries = {
 		});
 
 		AddMapBoundaries.boundariesFromGeoJsonLayer.addListener('click', AddMapBoundaries.boundTheMap);
+		AddMapBoundaries.boundariesFromGeoJsonLayer.addListener('click', function(e) {
+			/*TODO: clean up
+			Crude attempt to Show zoomed into the state in the visual (other states should either not be visible or maybe greyed out if possible)*/
+			AddMapBoundaries.boundariesFromGeoJsonLayer.setStyle({ //using set style we can set styles for all boundaries at once
+				fillColor: '#ddd',
+				strokeWeight: 1,
+				fillOpacity: 1
+			});
+			AddMapBoundaries.boundariesFromGeoJsonLayer.overrideStyle(e.feature, {
+				strokeWeight: 3,
+				strokeColor: '#ff0000',
+				fillColor: 'blue',
+				fillOpacity: 0.8
+			});
+		});
 
 		AddMapBoundaries.boundariesFromGeoJsonLayer.addListener('mouseover', function(e) {
 			AddMapBoundaries.boundariesFromGeoJsonLayer.overrideStyle(e.feature, {
