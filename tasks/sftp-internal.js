5c85ebe0104b0fc6cfefc376b7c2bcfd0935d9f7
Ian White <ian.white@t-3.com>
2016-08-19 17:33:28 -0500

    fix sftp task to use .ftppass file to hide credz

--- tasks/sftp-internal.js
+++ tasks/sftp-internal.js
@@ -3,5 +3,9 @@ var sftp = require('gulp-sftp');
  
 gulp.task('sftp-internal', function () {
     return gulp.src('public/**/*')
-        .pipe(sftp());
+        .pipe(sftp({
+        	host: '10.1.1.168',
+        	auth: 'privateKeyEncrypted',
+        	remotePath: '/var/www/webserve/7-election.internal-l.thethinktank.com'
+        }));
 });
