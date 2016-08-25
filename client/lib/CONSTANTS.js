f97667e9ac42a38eec0f64b94cd7fd3222166a62
Ian White <ian.white@t-3.com>
2016-08-25 13:40:32 -0500

    rename classes

--- client/lib/CONSTANTS.js
+++ client/lib/CONSTANTS.js
@@ -1,8 +1,8 @@
 let $ = require('jquery');
 let TEST_API_URL = 'https://api-test.7-eleven.com/v3/election/stores/';
-let redCupEl = $('.cup.red .percentage');
-let blueCupEl = $('.cup.blue .percentage');
-let purpleCupEl = $('.cup.purple .percentage')
+let redCupEl = $('.cup_results_container .percentage');
+let blueCupEl = $('.cup_results_container .percentage');
+let purpleCupEl = $('.cup_results_container .percentage')
 let bannerEl = $('.results .banner');
 let directionsEl = $('.results .directions');
 let stateSelectEl = $('.state_select');
