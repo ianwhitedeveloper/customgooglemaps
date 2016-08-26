b1fb8e9db8ca87283fad1cac9deed374ddb5d51d
Ian White <ian.white@t-3.com>
2016-08-25 19:45:47 -0500

    tweak body bg add color bubbles to percentages

--- client/lib/CONSTANTS.js
+++ client/lib/CONSTANTS.js
@@ -1,8 +1,8 @@
 let $ = require('jquery');
 let TEST_API_URL = 'https://api-test.7-eleven.com/v3/election/stores/';
-let redCupEl = $('.cup_results_container .percentage');
-let blueCupEl = $('.cup_results_container .percentage');
-let purpleCupEl = $('.cup_results_container .percentage')
+let redCupEl = $('.cup_results_container.red .percentage');
+let blueCupEl = $('.cup_results_container.blue .percentage');
+let purpleCupEl = $('.cup_results_container.purple .percentage')
 let bannerEl = $('.results .banner');
 let directionsEl = $('.results .directions');
 let stateSelectEl = $('.state_select');
