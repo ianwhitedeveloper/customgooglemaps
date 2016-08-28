3877934dff05f6d3e6111604ff85a6979e4ad47a
Ian White <ian.white@t-3.com>
2016-08-27 21:31:14 -0500

    add promise polyfill

--- client/lib/mapCenterChange.js
+++ client/lib/mapCenterChange.js
@@ -2,6 +2,7 @@ let map = require('../lib/map');
 let $ = require('jquery');
 let debounce = require('es6-promise-debounce');
 let sElEvtEmitter = require('./globals').sElEvtEmitter;
+let Promise = require('es6-promise').Promise;
 
 let debouncedFunction = debounce(function(center) {
     return new Promise(function(resolve) {
