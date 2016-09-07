10ad01d1ccb01acff8c57124931fb0b07b31a581
Ian White <ian.white@t-3.com>
2016-09-07 12:34:44 -0500

    redo logic to display city name based purely on google geocoder result of current map center lat lng instead of using what's returned from the 7 eleven stores API

--- client/lib/getStateAndCityNameFromGeoResults.js
+++ client/lib/getStateAndCityNameFromGeoResults.js
@@ -2,28 +2,23 @@ let $ = require('jquery');
 let sElEvtEmitter = require('./globals').sElEvtEmitter;
 let map = require('../lib/map');
 let STATE_ZOOM_LVL = require('./CONSTANTS').STATE_ZOOM_LVL;
-let RESULTS_ZOOM_LVL = require('./CONSTANTS').RESULTS_ZOOM_LVL;
 
 
 function getStateAndCityNameFromGeoResults(results) {
-	let deferred = $.Deferred();
-	// http://stackoverflow.com/questions/6778205/google-maps-geocoder-to-return-state
-	for(let i=0; i < results[0].address_components.length; i++)
-    {
-        if (results[0].address_components[i].types[0] === "administrative_area_level_1")
+    try {
+    	let deferred = $.Deferred();
+        let data = {};
+        // http://stackoverflow.com/questions/6778205/google-maps-geocoder-to-return-state
+        for(let i=0; i < results[0].address_components.length; i++)
         {
-            let data = {};
-
-            if (results[0].address_components.length > 2) {
-                data.cityName = results[0].address_components[0].long_name;
-                sElEvtEmitter.emit('updateCityMeta', data.cityName);
-
-                setTimeout(() => {
-                    if (map.getZoom() <= STATE_ZOOM_LVL) {
-                        map.setZoom(RESULTS_ZOOM_LVL);
-                    }
-                }, 100);
-            } else {
+            if (results[0].address_components[i].types[0] === "administrative_area_level_1")
+            {
+                data.stateNameShort = results[0].address_components[i].short_name;
+                data.stateNameLong = results[0].address_components[i].long_name;
+                sElEvtEmitter.emit('updateStateMeta', data.stateNameShort);
+                deferred.resolve(data);
+            }
+            if (results[0].address_components[i].types.indexOf('locality') === -1) {
                 // Attmept to prevent map from zooming in too 
                 // far and triggering cup results prematurely
                 setTimeout(() => {
@@ -33,16 +28,11 @@ function getStateAndCityNameFromGeoResults(results) {
                 }, 100);
                 sElEvtEmitter.emit('clearCityMeta');
             }
-
-            data.stateNameShort = results[0].address_components[i].short_name;
-            data.stateNameLong = results[0].address_components[i].long_name;
-
-            sElEvtEmitter.emit('updateStateMeta', data.stateNameShort);
-
-            deferred.resolve(data);
         }
+        return deferred.promise();
+    } catch(error) {
+        sElEvtEmitter.emit('silentError', error);
     }
-    return deferred.promise();
 }
 
 sElEvtEmitter.on('getStateAndCityNameFromGeoResults', getStateAndCityNameFromGeoResults);
